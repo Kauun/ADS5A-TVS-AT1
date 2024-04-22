fd9bd4b1e6f1ccb78f690515321adc09940586da
Kauan <kauandavifragagomes@gmail.com>
2024-04-21 22:34:17 -0300

    Atividade - Teste e Validação de Sftw

--- recebeDados.test.js
+++ recebeDados.test.js
@@ -2,9 +2,9 @@
 const dadosEstudante = require('./dadosEstudante');
 
 describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+  test('Verifica se a função processa corretamente o nome "Kauan Davi Fraga Gomes" e o RA "189675"', () => {
+    const nome = "Kauan Davi Fraga Gomes";
+    const RA = "189675";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
